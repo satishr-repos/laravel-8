3fdd46cada9e7bc5f2576b37128aad8ac854587f
unknown <satishjs09@gmail.com>
2021-10-03 12:34:29 +0530

    remove unused div class

--- hmfinplan/resources/js/components/Utils/SimpleCard.vue
+++ hmfinplan/resources/js/components/Utils/SimpleCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="max-w-xl">
+    <div class="max-w-xl box-border">
         <div class="bg-white rounded shadow-md w-full">
-            <div class="self-stretch">
+            <div class="">
                 <div class="w-full py-4 bg-purple-200 flex justify-between items-center">
                     <h2 class="pl-3 text-xl font-semibold text-gray-500">{{ title }}</h2>
                     <slot name="title"></slot>
                 </div>
             </div>    
-            <div class="self-strech">
+            <div class="">
                 <slot name="content"></slot>
             </div>
         </div>
